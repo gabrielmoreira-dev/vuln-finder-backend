0da9892e8231a33415a1812af9378968bc87e605
= <=>
2020-12-19 22:30:11 -0300

    Config test

--- src/controller/auth_controller.js
+++ src/controller/auth_controller.js
@@ -1,13 +1,20 @@
 const User = require('../data/model/user')
 
 module.exports = {
+
   registerUser: async (req, res) => {
-    const { email } = req.body
+    const { email, password } = req.body
 
     try {
       if (await User.findOne({ email })) {
         return res.status(400).send({
           error: 'User already registered'
+        })
+      }
+
+      if (password.length < 8) {
+        return res.status(400).send({
+          error: 'Invalid password format'
         })
       }
 
@@ -22,5 +29,33 @@ module.exports = {
         error: e.message
       })
     }
+  },
+
+  authenticateUser: async (req, res) => {
+
+    const { email, role } = req.body
+
+    try {
+      const user = User.findOne({ email })
+
+      if (!user) {
+        return res.status(400).send({
+          error: 'User not found'
+        })
+      }
+
+      if (user.role != role) {
+        return res.status(400).send({
+          error: 'Invalid permission'
+        })
+      }
+    }
+    catch (e) {
+      return res.status(500).send({
+        error: e.message
+      })
+    }
+
   }
+
 }
---
+++ test/controller/auth_controller.test.js
@@ -0,0 +1,231 @@
+const AuthController = require('../../src/controller/auth_controller')
+const User = require('../../src/data/model/user')
+
+jest.mock('../../src/data/model/user')
+
+const accessToken = 'ACCESSTOKEN'
+
+const res = {
+  send: (obj) => obj,
+  status: (_) => {
+    return {
+      send: (obj) => obj
+    }
+  }
+}
+
+let userList
+
+beforeEach(() => {
+  userList = [
+    {
+      id: 1,
+      name: 'Client',
+      email: 'client@test.com',
+      password: '12345678',
+      role: 'Client'
+    },
+    {
+      id: 2,
+      name: 'Professional',
+      email: 'professional@test.com',
+      password: '12345678',
+      role: 'Professional'
+    }
+  ]
+})
+
+describe('Register', () => {
+
+  beforeAll(() => {
+    User.findOne.mockImplementation(({ email }) => {
+      return userList.find(user => {
+        return user.email === email
+      })
+    })
+
+    User.create.mockImplementation((params) => {
+      userList.push({
+        id: userList.length + 1,
+        name: params.name,
+        email: params.email,
+        password: params.password,
+        role: params.role
+      })
+      return true
+    })
+  })
+
+  it('Should register a client user', async () => {
+    const user = {
+      name: 'Client',
+      email: 'client@gmail.com',
+      password: '12345678',
+      role: 'Client'
+    }
+
+    const req = { body: user }
+
+    const result = await AuthController.registerUser(req, res)
+    const userResult = await User.findOne({ email: 'client@gmail.com' })
+
+    expect(result['token']).toBeDefined()
+    expect(result).toEqual({ 'token': accessToken })
+    expect(userResult).toBeDefined()
+    expect(userResult.role).toBe('Client')
+  })
+
+  it('Should register a professional user', async () => {
+    const user = {
+      name: 'Professional',
+      email: 'professional@gmail.com',
+      password: '12345678',
+      role: 'Professional'
+    }
+
+    const req = { body: user }
+
+    const result = await AuthController.registerUser(req, res)
+    const userResult = await User.findOne({ email: 'professional@gmail.com' })
+
+    expect(result['token']).toBeDefined()
+    expect(result).toEqual({ 'token': accessToken })
+    expect(userResult).toBeDefined()
+    expect(userResult.role).toBe('Professional')
+  })
+
+  it('Should return an user already registered error', async () => {
+    const user = {
+      name: 'Professional',
+      email: 'professional@test.com',
+      password: '12345678',
+      role: 'Professional'
+    }
+
+    const req = { body: user }
+
+    const result = await AuthController.registerUser(req, res)
+
+    expect(result['token']).toBeUndefined()
+    expect(result['error']).toBeDefined()
+    expect(result).toEqual({ 'error': 'User already registered' })
+  })
+
+  it('Should return an invalid password format error', async () => {
+    const user = {
+      name: 'Professional',
+      email: 'professional@gmail.com',
+      password: '123456',
+      role: 'Professional'
+    }
+
+    const req = { body: user }
+
+    const result = await AuthController.registerUser(req, res)
+
+    console.log(result)
+
+    expect(result['token']).toBeUndefined()
+    expect(result['error']).toBeDefined()
+    expect(result).toEqual({ 'error': 'Invalid password format' })
+  })
+
+})
+
+describe('Authenticate', () => {
+
+  test('Should authenticate a client user', async () => {
+    const req = {
+      body: {
+        email: 'client@test.com',
+        password: '12345678',
+        role: 'Client'
+      }
+    }
+
+    const result = await AuthController.authenticateUser(req, res)
+
+    expect(result['token']).toBeDefined()
+    expect(result).toEqual({ 'token': accessToken })
+  })
+
+  test('Should authenticate a professional user', async () => {
+    const req = {
+      body: {
+        email: 'professional@test.com',
+        password: '12345678',
+        role: 'Professional'
+      }
+    }
+
+    const result = await AuthController.authenticateUser(req, res)
+
+    expect(result['token']).toBeDefined()
+    expect(result).toEqual({ 'token': accessToken })
+  })
+
+  test('Should not authenticate a client user as professional', async () => {
+    const req = {
+      body: {
+        email: 'client@test.com',
+        password: '12345678',
+        role: 'Professional'
+      }
+    }
+
+    const result = await AuthController.authenticateUser(req, res)
+
+    expect(result['token']).toBeUndefined()
+    expect(result['error']).toBeDefined()
+    expect(result).toEqual({ 'error': 'Invalid permission' })
+  })
+
+  test('Should not authenticate a professional user as client', async () => {
+    const req = {
+      body: {
+        email: 'professional@test.com',
+        password: '12345678',
+        role: 'Client'
+      }
+    }
+
+    const result = await AuthController.authenticateUser(req, res)
+
+    expect(result['token']).toBeUndefined()
+    expect(result['error']).toBeDefined()
+    expect(result).toEqual({ 'error': 'Invalid permission' })
+  })
+
+  test('Should return an user not found error', async () => {
+    const req = {
+      body: {
+        email: 'professional2@test.com',
+        password: '12345678',
+        role: 'Professional'
+      }
+    }
+
+    const result = await AuthController.authenticateUser(req, res)
+
+    expect(result['token']).toBeUndefined()
+    expect(result['error']).toBeDefined()
+    expect(result).toEqual({ 'error': 'User not found' })
+  })
+
+  test('Should return an invalid password error', async () => {
+    const req = {
+      body: {
+        email: 'professional@test.com',
+        password: '123456',
+        role: 'Professional'
+      }
+    }
+
+    const result = await AuthController.authenticateUser(req, res)
+
+    expect(result['token']).toBeUndefined()
+    expect(result['error']).toBeDefined()
+    expect(result).toEqual({ 'error': 'Invalid password' })
+  })
+
+})
